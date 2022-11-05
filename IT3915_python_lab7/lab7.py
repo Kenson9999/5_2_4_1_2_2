9b3612e59c03b5a664c558e7e6f4e0fdcd7ec5da
Kenson9999 <kensonchan38@gmail.com>
2022-11-05 21:07:32 +0800

    update_lab7

---
+++ IT3915_python_lab7/lab7.py
@@ -0,0 +1,9 @@
+list1 = [ "a", "b" ]
+list2 = [1, 2]
+list3 = list1 + list2
+print(list3)
+numbers = list ( range ( 5, 35, 10) )
+print(numbers)
+string = "hello how are you?"
+list1 = string.split()
+print(list1)
